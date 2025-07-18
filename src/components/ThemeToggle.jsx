12ad7af638bf3d76e67c9e47c386a97a39a1fdda
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-07-18 19:13:40 +0200

    Light/Dark mode toggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Moon, Sun } from "lucide-react"
+import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -28,8 +29,15 @@ export const ThemeToggle = () => {
         }
     }
 
-    return <button onClick={toggleTheme}> 
-        {" "}
-        {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>} 
-        </button>;
+    return (
+            <button 
+                onClick={toggleTheme}
+                className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full trabsition-colors duration-300",
+                    "focus:outlin-hidden"
+                )}
+            > 
+                {" "}
+                {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>} 
+            </button>
+        );
 };
