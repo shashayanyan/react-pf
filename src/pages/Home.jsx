c9f48824f1466159b2a558e0f9fb3f9a1374cb18
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-07-18 18:54:58 +0200

    Style Sheet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
-    return <div>Home</div>;
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/*Theme Toggle */}
+            <ThemeToggle />
+        {/*Background Effect */}
+
+        {/*Navbar */}
+
+        {/*Main */}
+
+        {/*Footer */}
+
+
+
+
+    </div>;
 };
