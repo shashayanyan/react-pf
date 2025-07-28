ffb47436eff310ca877490a196fbd1664b56fe18
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-07-28 12:45:44 +0200

    Hero Section setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -11,6 +12,9 @@ export const Home = () => {
         {/*Navbar */}
             <Navbar />
         {/*Main */}
+            <main>
+                <HeroSection />
+            </main>
 
         {/*Footer */}
 
