46912ccacaf2b2ef7fe9f9d65d2be428a19d4da9
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-07-28 13:12:33 +0200

    About section setup -- texts not correct

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -14,6 +15,7 @@ export const Home = () => {
         {/*Main */}
             <main>
                 <HeroSection />
+                <AboutSection />
             </main>
 
         {/*Footer */}
