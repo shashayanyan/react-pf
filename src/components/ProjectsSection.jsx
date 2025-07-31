be574d24c786e80fac4964c5defbf5b4dd8f1336
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-07-31 14:48:03 +0200

    Finished Project section setup

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,5 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react"
+
 const projects = [
     {
         id: 1,
@@ -42,11 +44,44 @@ export const ProjectsSection = () => {
                 {projects.map((project, key) => (
                     <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"> 
                         <div className="h-48 overflow-hidden">
-                            <img src={project.image} alt={project.title} />
+                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
                         </div> 
-                    
+
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        
+
+                            <h3 className="text-xl font-semibold mb-1">
+                                {project.title}
+                            </h3>
+                            <p className="text-muted-foreground text-sm mb-4">
+                                    {project.description}
+                            </p>
+                            <div className="flex justify-between items-center">
+                                    <div className="flex space-x-3 ">
+                                        <a href={project.gitUrl} className="text-foreground/80 center hover:text-primary transition-colors duration-300"
+                                            target="_blank">
+                                        <ExternalLink size={20} />  
+                                        </a>
+                                    </div>
+                            </div>
+                        </div>
                     </div>
                 ))}
+            </div>
+
+            <div className="text-center mt-12">
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2" 
+                target="_blank"
+                href="https://github.com/shashayanyan">
+                    Check My GitHub! <Github size={16}/>
+                </a>
             </div>
 
         </div>
