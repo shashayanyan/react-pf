0749a09b288935a79c780cb8bfad47f83bf7be91
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-08-05 16:03:04 +0200

    make dark theme default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,11 +3,11 @@ import { Moon, Sun } from "lucide-react"
 import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
     
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
-        if (storedTheme === "dark") {
+        if (storedTheme === "light") {
             setIsDarkMode(true);
             document.documentElement.classList.add("dark")
         } else {
