6115782ecc8c8ac283a494012df2a2c30ec1e1a3
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-08-06 14:52:04 +0200

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -8,11 +8,13 @@ export const ThemeToggle = () => {
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
         if (storedTheme === "light") {
-            setIsDarkMode(true);
-            document.documentElement.classList.add("dark")
-        } else {
-            localStorage.setItem("theme", "light");
             setIsDarkMode(false);
+            localStorage.setItem("theme", "light");
+            //document.documentElement.classList.remove("dark");
+        } else {
+            document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
         }
     }, []);
 
