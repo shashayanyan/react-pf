714dda10809358842b0dad1529b88cf840ed40c5
shashayanyan <47574687+shashayanyan@users.noreply.github.com>
2025-08-18 17:15:01 +0200

    download cv deployment attempt2

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,7 @@ export const AboutSection = () => {
                             Get In Touch!
                         </a>
 
-                        <a href="src\assets\shashayanyan_CV.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" download>
+                        <a href="src\assets\shashayanyan_CV.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" download="Shayan_CV.pdf">
                             Download CV
                         </a>
                     </div>
